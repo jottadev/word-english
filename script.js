1abbe22f5244b54fc2add72e20d97436b10bb525
jottadev <contato.joaoythalo@gmail.com>
2022-07-03 15:00:06 -0300

    Trabalho de Ingles

---
+++ script.js
@@ -0,0 +1,35 @@
+let comedy = [
+    {
+        title: 'Grows Up 2',
+        director: 'Dennis Dugan',
+        year: 2013
+    }
+]
+
+let action = [
+    {
+        title: 'The Fast and the Furious: Tokyo Drift',
+        director: 'Justin Lin',
+        year: 2006
+    }
+]
+
+let fantasy = [
+    {
+        title: 'Avengers: Infinity war',
+        director: ('Antony Russo', 'Joe Russo'),
+        year: 2018
+    }
+]
+
+let favMovie = 'Interestelar'
+
+if(favMovie === 5){
+    console.log('My Favorite movie')
+}else {
+    console.log('Not even 5 star is Red Notice')
+}
+
+
+
+
